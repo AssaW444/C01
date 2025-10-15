9adfa12f2c99042a9a49815a42e828e029078d43
Anthony <you@example.com>
2025-10-15 12:09:16 +0200

    ex00

--- ex00/ipssi_iterative_factorial.c
+++ ex00/ipssi_iterative_factorial.c
@@ -1,5 +1,22 @@
+#include <stdio.h>
+
 int ipssi_iterative_factorial(int nb)
 {
+	int	fact;
+ 
+	fact = 1;
+	if (nb < 0)
+		return 0;
+	while (nb >= 1)
+	{
+		fact = fact*nb;
+		nb--;
+	}
+	return fact;
+}
 
-
+int main ()
+{
+	printf("%d\n",ipssi_iterative_factorial(2));
+	return 1;
 }
