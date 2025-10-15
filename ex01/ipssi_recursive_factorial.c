8f11ff18510234b30a4a28bcd84b7757c1cfa4a2
Anthony <you@example.com>
2025-10-15 12:26:04 +0200

    git ignore

--- ex01/ipssi_recursive_factorial.c
+++ ex01/ipssi_recursive_factorial.c
@@ -1,6 +1,16 @@
-int ipssi_recursive_factorial(int nb)
+#include <stdio.h>
+
+int	ipssi_recursive_factorial(int nb)
 {
+	int	fact;
 
-
-
+	fact = 1;
+	if (nb < 0)
+		return (0);
+	while (nb >= 1)
+	{
+		fact = fact * nb;
+		nb--;
+	}
+	return (fact);
 }
